7f9acc5cef830fecbe43c21d8f08d1e2f12a903b
viks <victoria.miledina@gmail.com>
2023-11-21 21:49:22 +0100

    fix: fix accesibility issues

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
+import { resolve } from "path";
+
 export default {
   root: "./src",
   build: {
     outDir: "../dist",
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        catalog: resolve(__dirname, "src/catalog.html"),
+        // 404: resolve(__dirname, "src/404.html"),
+      },
+    },
   },
 };
